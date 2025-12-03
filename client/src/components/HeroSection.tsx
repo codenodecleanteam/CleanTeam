1d1eee1da33efa2543bc08e8d79a3d5e415b2a73
Daniel Correia <devdanielcorreia@gmail.com>
2025-12-03 00:11:19 -0300

    feat: atualizar imagem do hero

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import { Button } from '@/components/ui/button';
 import { ArrowRight, Play } from 'lucide-react';
-import heroImage from '@assets/generated_images/cleaning_team_nyc_hero.png';
+import heroImage from '@assets/generated_images/ImagemHeroCleanTeam.png';
 
 interface HeroSectionProps {
   onStartTrial?: () => void;
